670fb41b301cab6d7a0a9f3657b0197bf736a451
Jean <m.neyraudveil@gmail.com>
2025-11-07 01:52:41 +0100

    made some changes

--- ChessEngine/Core/Core.h
+++ ChessEngine/Core/Core.h
@@ -12,23 +12,11 @@
 
 #include "definition.h"
 
-/*
-	The Controller send a request with a selected piece and a target position
-	Check if the move is legal
-	if legal we move the piece
-		else we return an error
-
-	The Controller send a request for possible moves for a selected piece
-	Check all the legal moove for this piece
-	return a list of possible moves
-*/
-
-
 class Core {
 
 public:
 	explicit Core();
-	~Core() {};
+	~Core() = default;
 
 	void debugDisplayChessBoard() const;
 	bool isMoveLegal(const Vec2& from, const Vec2& to) const;
@@ -36,8 +24,8 @@ public:
 	bool movePiece(const Vec2& from, const Vec2& to);
 	std::vector<Vec2> getPossibleMoves(const Vec2& from) const;
 
-	[[nodiscard]] inline const BoardCell& At(const Vec2& pos) const { return chessBoard[pos.y][pos.x]; }
-	[[nodiscard]] inline BoardCell& At(const Vec2& pos) { return chessBoard[pos.y][pos.x]; }
+	[[nodiscard]] const BoardCell& At(const Vec2& pos) const { return chessBoard[pos.y][pos.x]; }
+	[[nodiscard]] BoardCell& At(const Vec2& pos) { return chessBoard[pos.y][pos.x]; }
 
 private:
 	void fillChessBoard();
